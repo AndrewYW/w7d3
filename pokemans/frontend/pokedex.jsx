ca35d752fef8529e4be7135857ec4a61984cd478
AndrewYW <andrewwwang39@gmail.com>
2019-01-16 16:46:08 -0500

    finish phase 2

--- pokemans/frontend/pokedex.jsx
+++ pokemans/frontend/pokedex.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { receiveAllPokemon } from './actions/pokemon_actions';
+import { receiveAllPokemon, requestAllPokemon } from './actions/pokemon_actions';
 import * as APIUtil from './util/api_util';
-import configureStore from './store/store'
+import configureStore from './store/store';
+import { selectAllPokemon } from './reducers/selectors';
 
 document.addEventListener('DOMContentLoaded', () => {
   
@@ -12,9 +13,18 @@ document.addEventListener('DOMContentLoaded', () => {
 
   window.getState = window.store.getState;
   window.dispatch = window.store.dispatch;
+  window.selectAllPokemon = selectAllPokemon;
 
   window.fetchAllPokemon = APIUtil.fetchAllPokemon;
   window.receiveAllPokemon = receiveAllPokemon;
+  window.requestAllPokemon = requestAllPokemon;
   
   ReactDOM.render(<h1>Pokedex</h1>, rootEl);
 });
+// const initialState = getState();
+// selectAllPokemon(initialState); //=> []
+
+// dispatch(requestAllPokemon());
+
+// const populatedState = getState();
+// selectAllPokemon(populatedState); //
--- pokemans/frontend/reducers/pokemon_reducer.js
+++ pokemans/frontend/reducers/pokemon_reducer.js
@@ -1,11 +1,17 @@
 import { RECEIVE_ALL_POKEMON } from "../actions/pokemon_actions";
 import { merge } from 'lodash';
+
 const pokemonReducer = (state = {}, action) => {
   const newState = merge({}, state);
+
   switch(action.type){
     case RECEIVE_ALL_POKEMON:
-      return merge(newState, action.pokemon);
       
+      Object.values(action.pokemon).forEach( (poke) => {
+        // debugger
+        newState[poke.id] = poke;
+      });
+    return newState;
     default:
       return state;
   }
